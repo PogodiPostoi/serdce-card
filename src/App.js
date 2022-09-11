8b729372e19181aa445248874f9bd84fe9a3dfc0
PogodiPostoi <boombox577@gmail.com>
2022-09-11 17:29:06 +0300

    new page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
 import React from "react";
 
-import Cards from "./pages/Cards";
-
-import Header from "./components/Header";
-import CustomCard from "./components/CustomCard";
-import SimpleCard from "./components/SimpleCard";
-
-// import CustomCardGrid from "./pages/CustomCardGrid";
-
-export const AppContext = React.createContext({});
-
 function App() {
   return (
-    <AppContext.Provider value={1}>
-      <div className="wrapper">
-        <Header />
-        <Cards />
-      </div>
-    </AppContext.Provider>
+    1
   );
 }
 
