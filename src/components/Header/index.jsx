a47dadaa2b207a8b402f86a3ed740c9ac59f2ae5
PogodiPostoi <boombox577@gmail.com>
2022-09-12 18:44:52 +0300

    done with header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,13 +5,20 @@ const Header = () => {
         <>
         <div className={styles.header}>
             <div className={styles.headerLogo}>
-                <img src="./img/logo.svg" alt="logo" />
+                <img src="./img/header/logo.svg" alt="logo" />
                 <div className={styles.headerLogoContent}>
                     <h2>Serdce.cards</h2>
                     <p>Лучшие электронные открытки</p>
                 </div>
             </div>
-            <div className={styles.headerBar}></div>
+            <div className={styles.headerBar}>
+                <div className={styles.headerBarCart}>
+                    <img src="./img/header/cart.svg" alt="cart" />
+                    <p className={styles.headerBarCartText}>258 руб.</p>
+                </div>
+                <img className={styles.headerBarFavorite} src="./img/header/favorite.svg" alt="favorite" />
+                <img className={styles.headerBarProfile} src="./img/header/profile.svg" alt="profile" />
+            </div>
         </div>
         <div className={styles.line}></div>
         </>
