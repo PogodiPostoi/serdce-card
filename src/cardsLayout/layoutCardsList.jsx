ba19685a248dbaccfef72d1a40e9bfb1687c3fc3
PogodiPostoi <boombox577@gmail.com>
2022-10-03 23:49:26 +0300

    new card giraffe

--- src/cardsLayout/layoutCardsList.jsx
+++ src/cardsLayout/layoutCardsList.jsx
@@ -10,38 +10,11 @@ const CardListItems = [
     },
     {
         'id': 2,
-        'name': 'thief_card',
-        'title': 'Воришка1',
-        'front_img': './img/cards/thief-card/thief_front.png',
-        'back_img': './img/cards/thief-card/thief_back.png',
+        'name': 'cute_but_card',
+        'title': 'Милый, но..',
+        'front_img': './img/cards/giraffe/giraffe_front.png',
+        'back_img': './img/cards/giraffe/giraffe_back.png',
         'price': 132,
-        'card_type': 1
-    },
-    {
-        'id': 3,
-        'name': 'thief_card',
-        'title': 'Воришка2',
-        'front_img': './img/cards/thief-card/thief_front.png',
-        'back_img': './img/cards/thief-card/thief_back.png',
-        'price': 114,
-        'card_type': 1
-    },
-    {
-        'id': 4,
-        'name': 'thief_card',
-        'title': 'Воришка3',
-        'front_img': './img/cards/thief-card/thief_front.png',
-        'back_img': './img/cards/thief-card/thief_back.png',
-        'price': 155,
-        'card_type': 1
-    },
-    {
-        'id': 5,
-        'name': 'thief_card',
-        'title': 'Воришка4',
-        'front_img': './img/cards/thief-card/thief_front.png',
-        'back_img': './img/cards/thief-card/thief_back.png',
-        'price': 100,
         'card_type': 1
     }
 ]
