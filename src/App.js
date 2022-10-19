26ae78a08b30916ee51a73017224ab97a5839e5e
PogodiPostoi <boombox577@gmail.com>
2022-10-19 19:19:29 +0300

    added routing

--- src/App.js
+++ src/App.js
@@ -1,39 +1,19 @@
 import React from "react";
 import axios from "axios";
+import { 
+  createBrowserRouter, 
+  RouterProvider, 
+  Route, Routes, Router, createRoutesFromElements } from "react-router-dom";
 
-import Header from "./components/Header"
-// import Store from "./pages/Store"
-import Favorites from "./pages/Favorites"
+import Header from "./components/Header";
+import Store from "./pages/Store"
+import Favorites from "./pages/Favorites";
 
 function App() {
-
-  // const [CardListItems, setCardListItems] = React.useState([])
+  const [CardListItems, setCardListItems] = React.useState([])
   const [favoriteListItems, setFavoriteListItems] = React.useState([]);
   const [cardDemoOpened, setCardDemoOpened] = React.useState(false);
   const [demoCardId, setDemoCardId] = React.useState("");
-
-  // Запрос открыток с сервера
-  React.useEffect(() => {
-    async function fetchData() {
-      try {
-        // const cardListResponse = await axios.get(
-        //   "https://634afa40d90b984a1e340df0.mockapi.io/cardListItems"
-        //   )
-        const favoriteListResponse = await axios.get(
-          "https://634afa40d90b984a1e340df0.mockapi.io/favoriteListItems"
-        );
-
-        // setCardListItems(cardListResponse.data)
-        setFavoriteListItems(favoriteListResponse.data);
-        
-      } catch (error) {
-        alert("Ошибка при запросе данных :(")
-        console.error(error)
-      }
-    }
-
-    fetchData()
-  }, [])
 
   const onToggleCardDemo = (id) => {
     setCardDemoOpened(!cardDemoOpened);
@@ -41,23 +21,65 @@ function App() {
     console.log(cardDemoOpened);
   };
 
+  // Запрос открыток с сервера
+  React.useEffect(() => {
+    async function fetchData() {
+      try {
+        const cardListResponse = await axios.get(
+          "https://634afa40d90b984a1e340df0.mockapi.io/cardListItems"
+          )
+        const favoriteListResponse = await axios.get(
+          "https://634afa40d90b984a1e340df0.mockapi.io/favoriteListItems"
+        );
+
+        setCardListItems(cardListResponse.data)
+        setFavoriteListItems(favoriteListResponse.data);
+      } catch (error) {
+        alert("Ошибка при запросе данных :(");
+        console.error(error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  // Router
+  const router = createBrowserRouter(
+    [
+      {
+        path: "/",
+        element: <Header/>,
+        children: [
+          {
+            path: "/",
+            element: <Store 
+              CardListItems = {CardListItems}
+              onToggleCardDemo = {onToggleCardDemo}
+              demoCardId = {demoCardId}
+              cardDemoOpened = {cardDemoOpened}
+            />
+            // Для случаев, когда нужно добавить страницу errorPage - errorElement: <Компонент />
+          },
+          {
+            path: "/favorites",
+            element: <Favorites 
+              favoriteListItems={favoriteListItems}
+              onToggleCardDemo={onToggleCardDemo}
+              demoCardId={demoCardId}
+              cardDemoOpened={cardDemoOpened}
+            />
+          }
+        ]
+        // Для случаев, когда нужно добавить страницу errorPage - errorElement: <Компонент />
+      }
+    ]
+  )
+
+  
+
   return (
     <div className="app">
-      
-      <Header />
-      {/* <Store 
-        CardListItems = {CardListItems}
-        onToggleCardDemo = {onToggleCardDemo}
-        demoCardId = {demoCardId}
-        cardDemoOpened = {cardDemoOpened}
-      /> */}
-      <Favorites 
-        favoriteListItems = {favoriteListItems}
-        onToggleCardDemo = {onToggleCardDemo}
-        demoCardId = {demoCardId}
-        cardDemoOpened = {cardDemoOpened}
-      />
-      
+      <RouterProvider router={router} />
     </div>
   );
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,26 +1,32 @@
+import { Link, Outlet } from "react-router-dom";
 import styles from "./Header.module.scss";
 
 const Header = () => {
     return(
         <>
-        <div className={styles.header}>
-            <div className={styles.headerLogo}>
-                <img src="./img/header/logo.svg" alt="logo" />
-                <div className={styles.headerLogoContent}>
-                    <h2>Serdce.cards</h2>
-                    <p>Электронное счастье</p>
+            <div className={styles.header}>
+                <Link to={"/"} className={styles.headerLogoLink}>
+                    <div className={styles.headerLogo}>
+                        <img src="./img/header/logo.svg" alt="logo" />
+                        <div className={styles.headerLogoContent}>
+                            <h2>Serdce.cards</h2>
+                            <p>Электронное счастье</p>
+                        </div>
+                    </div>
+                </Link>
+                <div className={styles.headerBar}>
+                    <div className={styles.headerBarCart}>
+                        <img src="./img/header/cart.svg" alt="cart" />
+                        <p className={styles.headerBarCartText}>258 руб.</p>
+                    </div>
+                    <Link to={"/favorites"}>
+                        <img className={styles.headerBarFavorite} src="./img/header/favorite.svg" alt="favorite" />
+                    </Link>
+                    <img className={styles.headerBarProfile} src="./img/header/profile.svg" alt="profile" />
                 </div>
             </div>
-            <div className={styles.headerBar}>
-                <div className={styles.headerBarCart}>
-                    <img src="./img/header/cart.svg" alt="cart" />
-                    <p className={styles.headerBarCartText}>258 руб.</p>
-                </div>
-                <img className={styles.headerBarFavorite} src="./img/header/favorite.svg" alt="favorite" />
-                <img className={styles.headerBarProfile} src="./img/header/profile.svg" alt="profile" />
-            </div>
-        </div>
-        <div className={styles.line}></div>
+            <div className={styles.line}></div>
+            <Outlet />
         </>
     )
 }
